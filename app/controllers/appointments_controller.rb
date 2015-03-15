32058cf4fc7371bbbce53195ef33f1d53bdafe32
Annie Lydens <aelydens@gmail.com>
2015-03-15 14:24:43 -0600

    User can add appointments on dog#show

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,5 +1,20 @@
 class AppointmentsController < ApplicationController
   def create
-    
+    #@dog = Dog.find(params[:dog_id])
+    #@appointment = Appointment.new(appointment_params.merge(dog_id: @dog.id))
+    @appointment = Appointment.new(appointment_params.merge(dog_id: params[:dog_id]))
+    if @appointment.save
+      flash[:notice] = "You've made an appointment"
+      redirect_to dog_path(params[:dog_id])
+    else
+      render "dogs/show"
+    end
   end
+
+  private
+
+  def appointment_params
+    params.require(:appointment).permit(:time, :vet_id)
+  end
+
 end
